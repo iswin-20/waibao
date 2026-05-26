ab9a1315e4ff3b2de3678fbed27b98aa4b19df37
iswin-20 <leid0711@gmail.com>
2026-05-26 23:34:44 +0800

    Point mini program API to server IP

--- miniprogram/env.js
+++ miniprogram/env.js
@@ -1,8 +1,6 @@
 module.exports = {
-  API_BASE_URL: 'http://localhost:3001',
+  API_BASE_URL: 'http://43.128.155.3:3000',
   API_BASE_URLS: [
-    'http://localhost:3001',
-    'http://127.0.0.1:3001',
-    'http://192.168.1.4:3001'
+    'http://43.128.155.3:3000'
   ]
 };
